d2860b004ffb31ddc3a64c62af14d0e694d68db6
Ester-0 <azalea_@hotmail.com>
2023-10-23 21:33:28 +0200

    ok

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // ¿En tu páis de origen es normal dejar propina?
+
+const calcButton = document.querySelector("#calculate");
+
+calcButton.addEventListener('click', function () {
+    const billAmount = +document.querySelector("#bill-amount").value;
+    const percentageTip = +document.querySelector("#percentage-tip").value;
+
+    const tipAmount = billAmount * percentageTip / 100;
+    const total = billAmount + tipAmount;
+
+    document.querySelector("#tip-amount").value = tipAmount;
+    document.querySelector("#total").value = total;
+
+});
